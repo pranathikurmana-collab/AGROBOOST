764aa3366a5c4ee68901ef18c4f14d2018cbe5d4
pranathikurmana-collab <pranathikurmana@gmail.com>
2026-04-21 15:54:58 +0530

    updated

--- project/script.js
+++
@@ -1,4 +0,0 @@
-// script.js – AgroBoost shared utilities
-
-// All page-specific logic is in inline <script> tags in each HTML file.
-// This file is kept for any future shared functions you may want to add.
